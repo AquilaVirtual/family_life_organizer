072bb589dcd46797a5b667907619edfb3b9e8290
David Loveday <davemech@yahoo.com>
2019-01-02 14:46:21 -0800

    move navigation out of App.js into a separate component

--- family_life_frontend/src/App.js
+++ family_life_frontend/src/App.js
@@ -10,6 +10,7 @@ import ChorePage from "./components/chores/ChorePage";
 import AssignmentPage from "./components/assignments/AssignmentPage";
 import Login from "./components/user/Login";
 import Register from "./components/user/Register";
+import Navbar from "./components/navbar/Navbar";
 
 import './App.css';
 
@@ -17,28 +18,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* Temporary links */}
-        <Menu>
-          <Menu.Item>
-            <NavLink to="/">Home</NavLink>
-          </Menu.Item>
-          <Menu.Item>
-            <NavLink to="/users">Users</NavLink>
-          </Menu.Item>
-          <Menu.Item>
-            <NavLink to="/activities">Activities</NavLink>
-          </Menu.Item>
-          <Menu.Item>
-            <NavLink to="/calendar">Calendar</NavLink>
-          </Menu.Item>
-          <Menu.Item>
-            <NavLink to="/chores">Chores</NavLink>
-          </Menu.Item>
-          <Menu.Item>
-            <NavLink to="/homeworks">Homeworks</NavLink>
-          </Menu.Item>
-        </Menu>
-
+        <Navbar />
         {/* Available routes */}
         <Route exact path="/" component={LandingPage} />
         <Route exact path="/login" component={Login} />
