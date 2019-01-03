7b870521055c36ad98d865934e82149cf49f10c0
ykrueng <yasirah.krueng@gmail.com>
2019-01-03 15:11:36 -0600

    add functionality to change assignment status

--- family_life_frontend/src/components/assignments/AssignmentPage.js
+++ family_life_frontend/src/components/assignments/AssignmentPage.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Segment, Header, Button } from 'semantic-ui-react';
+import React from "react";
+import { Segment, Header, Button } from "semantic-ui-react";
 
 import { homework } from "../../dummyData";
 
@@ -9,19 +9,34 @@ import AssignmentCard from "./AssignmentCard";
 class AssignmentPage extends React.Component {
   state = {
     assignments: []
-  }
+  };
 
   componentDidMount() {
     this.setState({
-      assignments: homework,
-    })
+      assignments: homework
+    });
   }
 
-  deleteAssignment = (id) => {
+  deleteAssignment = id => {
     this.setState(state => ({
-      assignments: state.assignments.filter((assignment, i) => i!==id)
-    }))
-  }
+      assignments: state.assignments.filter((assignment, i) => i !== id)
+    }));
+  };
+
+  changeStatus = id => {
+    this.setState(state => ({
+      assignments: state.assignments.map((assignment, i) => {
+        if (id === i) {
+          return {
+            ...assignment,
+            status:
+              assignment.status === "initial" ? "incomplete" : "completed"
+          };
+        }
+        return assignment;
+      })
+    }));
+  };
 
   render() {
     const { assignments } = this.state;
@@ -29,27 +44,30 @@ class AssignmentPage extends React.Component {
       <Segment>
         <Navbar />
         <Header as="h2">Assignment Page</Header>
-        <Button icon="add" primary content="New Assignment"/>
-        <div style={{
-          maxWidth: "80rem",
-          margin: "1rem auto",
-          display: "flex",
-          alignItems: "flex-start",
-          justifyContent: "center",
-          flexWrap: "wrap"
-        }}>
-          {
-            assignments.map((assignment, i) => (
-              <AssignmentCard key={i} assignment={assignment}
-                deleteAssignment={this.deleteAssignment}
-                id={i}
-              />
-            ))
-          }
+        <Button icon="add" primary content="New Assignment" />
+        <div
+          style={{
+            maxWidth: "80rem",
+            margin: "1rem auto",
+            display: "flex",
+            alignItems: "flex-start",
+            justifyContent: "center",
+            flexWrap: "wrap"
+          }}
+        >
+          {assignments.map((assignment, i) => (
+            <AssignmentCard
+              key={i}
+              assignment={assignment}
+              deleteAssignment={this.deleteAssignment}
+              changeStatus={this.changeStatus}
+              id={i}
+            />
+          ))}
         </div>
       </Segment>
     );
   }
 }
- 
+
 export default AssignmentPage;
