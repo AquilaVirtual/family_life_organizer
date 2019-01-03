f6ab49707873c905202bdc395582501a50733495
ykrueng <yasirah.krueng@gmail.com>
2019-01-03 12:50:45 -0600

    add assignment card

--- family_life_frontend/src/components/assignments/AssignmentPage.js
+++ family_life_frontend/src/components/assignments/AssignmentPage.js
@@ -1,14 +1,46 @@
 import React from 'react';
-import { Segment, Header } from 'semantic-ui-react';
-import Navbar from "../navbar/Navbar";
+import { Segment, Header, Button } from 'semantic-ui-react';
 
-const AssignmentPage = () => {
-  return (
-    <Segment>
-    <Navbar />
-      <Header as="h2">Activity Page</Header>
-    </Segment>
-  );
+import { homework } from "../../dummyData";
+
+import Navbar from "../navbar/Navbar";
+import AssignmentCard from "./AssignmentCard";
+
+class AssignmentPage extends React.Component {
+  state = {
+    assignments: []
+  }
+
+  componentDidMount() {
+    this.setState({
+      assignments: homework,
+    })
+  }
+
+  render() {
+    const { assignments } = this.state;
+    return (
+      <Segment>
+        <Navbar />
+        <Header as="h2">Assignment Page</Header>
+        <Button icon="add" primary circular content="New Assignment"/>
+        <div style={{
+          maxWidth: "80rem",
+          margin: "1rem auto",
+          display: "flex",
+          alignItems: "flex-start",
+          justifyContent: "center",
+          flexWrap: "wrap"
+        }}>
+          {
+            assignments.map((assignment, i) => (
+              <AssignmentCard key={i} assignment={assignment} />
+            ))
+          }
+        </div>
+      </Segment>
+    );
+  }
 }
  
 export default AssignmentPage;
