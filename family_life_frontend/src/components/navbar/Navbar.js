8ea2dc6f0dcbc3f81fd50c9a4242c0588267f198
leiannel <leilouis112@gmail.com>
2019-01-03 15:32:06 -0500

    navbar completed

--- family_life_frontend/src/components/navbar/Navbar.js
+++ family_life_frontend/src/components/navbar/Navbar.js
@@ -10,7 +10,7 @@ const LandingPage = () => {
       <div><NavLink className="navbar--btn navbar--btn-activities"  to="/activities">Activities</NavLink></div>
       <div><NavLink className="navbar--btn navbar--btn-calendar"  to="/calendar">Calendar</NavLink></div>
       <div><NavLink className="navbar--btn navbar--btn-chores" to="/chores">Chores</NavLink></div>
-      <div><div ><NavLink className="navbar--btn navbar--btn-homework" to="/homeworks">Homeworks</NavLink></div></div>
+      <div><div ><NavLink className="navbar--btn navbar--btn-homework" to="/homeworks">Assignments</NavLink></div></div>
     </div>
   );
 };
