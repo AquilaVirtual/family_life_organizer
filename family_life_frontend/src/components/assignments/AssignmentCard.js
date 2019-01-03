1e4d032baee5987c2995d387071fb4f7b07fb872
ykrueng <yasirah.krueng@gmail.com>
2019-01-03 13:41:03 -0600

    add functionality to delete assignment

--- family_life_frontend/src/components/assignments/AssignmentCard.js
+++ family_life_frontend/src/components/assignments/AssignmentCard.js
@@ -1,27 +1,55 @@
 import React from "react";
-import { Segment, Header, Icon } from "semantic-ui-react";
+import { Segment, Header, Icon, Confirm } from "semantic-ui-react";
 
-const AssignmentCard = ({ assignment }) => {
-  return (
-    <Segment style={{ width: "18rem", margin: "1rem" }}>
-      <Header textAlign="center" as="h2">
-        {`${assignment.user}'s ${assignment.title}`}
-      </Header>
-      <p>
-        <Icon className={`${assignment.status === "completed" ? "check green" : "time yellow"}`} />
-        {assignment.status}
-      </p>
-      <div style={{ widht: "100%", textAlign: "left" }}>
-        <p>Due: {assignment.due.toDateString()}</p>
-        <p>
+class AssignmentCard extends React.Component{
+  state= {
+    confirmDelete: false,
+  }
+  render() {
+    const { assignment, deleteAssignment, id } = this.props;
+    const { confirmDelete } = this.state;
+    return (
+      <Segment style={{ width: "18rem", margin: "1rem" }}>
+        <Header textAlign="center" as="h2">
+          {`${assignment.user}'s ${assignment.title}`}
+        </Header>
+        <p style={{ fontSize: "1.4rem" }}>
+          <Icon
+            className={`${
+              assignment.status === "completed" ? "check green" : "time yellow"
+              }`}
+            style={{ cursor: "pointer", margin: "0 .5rem" }}
+          />
+          <Icon
+            style={{ cursor: "pointer", margin: "0 .5rem" }}
+            className="edit green"
+          />
+          <Icon
+            style={{ cursor: "pointer", margin: "0 .5rem" }}
+            className="trash alternate outline red"
+            onClick={() => this.setState({confirmDelete: true})}
+          />
+          <Confirm open={confirmDelete}
+            size="mini"
+            onCancel={() => this.setState({ confirmDelete: false })}
+            content={`Remove ${assignment.user}'s ${assignment.title}?`}
+            onConfirm={() => {
+              deleteAssignment(id);
+              this.setState({ confirmDelete: false })}
+            }
+          />
         </p>
-        <p>
-          Description:
+        <div style={{ widht: "100%", textAlign: "left" }}>
+          <p>Due: {assignment.due.toDateString()}</p>
+          <p />
+          <p>
+            Description:
           <br /> {assignment.description}
-        </p>
-      </div>
-    </Segment>
-  );
+          </p>
+        </div>
+      </Segment>
+    );
+  }
 };
 
 export default AssignmentCard;
