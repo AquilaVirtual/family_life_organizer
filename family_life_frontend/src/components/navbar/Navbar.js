32b5b1f40ba64d7bc94231a1cf3f2b165ee85cdb
ykrueng <yasirah.krueng@gmail.com>
2019-01-02 18:40:41 -0600

    update navbar

--- family_life_frontend/src/components/navbar/Navbar.js
+++ family_life_frontend/src/components/navbar/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Menu, Button } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
 
-const LandingPage = () => {
+const LandingPage = ({ history }) => {
   return (
     <div className="navbar">
       <Menu>
@@ -24,6 +24,14 @@ const LandingPage = () => {
         <Menu.Item>
           <NavLink to="/homeworks">Homeworks</NavLink>
         </Menu.Item>
+        <Menu.Menu position="right">
+            <Button primary onClick={() => history.push("/login")}>
+              Login
+            </Button>
+            <Button primary onClick={() => history.push("/register")}>
+              Register
+            </Button>
+        </Menu.Menu>
       </Menu>
     </div>
   );
