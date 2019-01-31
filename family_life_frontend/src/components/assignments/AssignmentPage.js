66ae9ba0b162a4e89983836462c93a5c29480a8a
David Loveday <davemech@yahoo.com>
2019-01-30 23:59:51 -0800

    format doc

--- family_life_frontend/src/components/assignments/AssignmentPage.js
+++ family_life_frontend/src/components/assignments/AssignmentPage.js
@@ -5,32 +5,33 @@ import axios from "axios";
 
 import Navbar from "../navbar/Navbar";
 import AssignmentCard from "./AssignmentCard";
-import SiteHeader from '../header/SiteHeader';
+import SiteHeader from "../header/SiteHeader";
 import AssignmentModal from "./AssignmentModal";
 
 class AssignmentPage extends React.Component {
   constructor(props) {
     super(props);
-  this.state = {
-    assignments: [],
-    modal: false,
-  };
-}
+    this.state = {
+      assignments: [],
+      modal: false
+    };
+  }
   componentDidMount() {
-    const token = localStorage.getItem('token');
-    const username = localStorage.getItem("username")
-    console.log("Getting some token", token)
-    const headers = { "headers": { "authorization": token } };
-    axios.get(`http://localhost:3002/api/assignment/${username}`, headers)
-    .then(response => {
-      console.log("Getting Assignment", response.data)
-      this.setState({
-        assignments: response.data
+    const token = localStorage.getItem("token");
+    const username = localStorage.getItem("username");
+    console.log("Getting some token", token);
+    const headers = { headers: { authorization: token } };
+    axios
+      .get(`http://localhost:3002/api/assignment/${username}`, headers)
+      .then(response => {
+        console.log("Getting Assignment", response.data);
+        this.setState({
+          assignments: response.data
+        });
+      })
+      .catch(err => {
+        console.log("Something Bahd!", err);
       });
-    })
-    .catch(err => {
-      console.log("Something Bahd!", err)
-    })   
   }
 
   handleModalToggle = (action, member) => {
@@ -40,40 +41,40 @@ class AssignmentPage extends React.Component {
       member
     }));
   };
-  deleteAssignment = id => { 
-
-    console.log("Some ID here", id)
-    axios.delete(`http://localhost:3002/api/assignment/${id}`)
-    .then(response => {
-      console.log("Deleted Assignment", response)
-    })
-    .catch(err => {
-      console.log("Something Bahd!", err)
-    })  
+  deleteAssignment = id => {
+    console.log("Some ID here", id);
+    axios
+      .delete(`http://localhost:3002/api/assignment/${id}`)
+      .then(response => {
+        console.log("Deleted Assignment", response);
+      })
+      .catch(err => {
+        console.log("Something Bahd!", err);
+      });
   };
- 
-  addAssignment = assignment => {
-    const token = localStorage.getItem('token');
-    console.log("Getting some token", token)
-    const headers = { "headers": { "authorization": token } };
 
-    console.log("Passing on", assignment)
-    axios.post(`http://localhost:3002/api/assignment/create`, assignment, headers)
-    .then(response => {
-      console.log("Logging Assignment", response)
-    })
-    .catch(err => {
-      console.log("Something Bahd!", err)
-    })   
-  }
+  addAssignment = assignment => {
+    const token = localStorage.getItem("token");
+    console.log("Getting some token", token);
+    const headers = { headers: { authorization: token } };
+
+    console.log("Passing on", assignment);
+    axios
+      .post(`http://localhost:3002/api/assignment/create`, assignment, headers)
+      .then(response => {
+        console.log("Logging Assignment", response);
+      })
+      .catch(err => {
+        console.log("Something Bahd!", err);
+      });
+  };
   changeStatus = id => {
     this.setState(state => ({
       assignments: state.assignments.map((assignment, i) => {
         if (id === i) {
           return {
             ...assignment,
-            status:
-              assignment.status === "initial" ? "incomplete" : "completed"
+            status: assignment.status === "initial" ? "incomplete" : "completed"
           };
         }
         return assignment;
@@ -84,9 +85,21 @@ class AssignmentPage extends React.Component {
   render() {
     const { assignments, modal } = this.state;
     return (
-      <Segment style={{textAlign: "center", border: 'none', boxShadow: '0px 0px 0px', height: '100vh', padding: '0px 0px'}}>
+      <Segment
+        style={{
+          textAlign: "center",
+          border: "none",
+          boxShadow: "0px 0px 0px",
+          height: "100vh",
+          padding: "0px 0px"
+        }}
+      >
         <Navbar />
-        <SiteHeader name="Assignments" />        <Button icon="add" primary content="New Assignment"
+        <SiteHeader name="Assignments" />{" "}
+        <Button
+          icon="add"
+          primary
+          content="New Assignment"
           onClick={this.handleModalToggle}
         />
         <div
@@ -109,7 +122,8 @@ class AssignmentPage extends React.Component {
             />
           ))}
         </div>
-        <AssignmentModal open={modal}
+        <AssignmentModal
+          open={modal}
           handleModalToggle={this.handleModalToggle}
           addAssignment={this.addAssignment}
         />
