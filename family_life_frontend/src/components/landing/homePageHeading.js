00825205a13e4c11665cfe94c74d4d5899b75070
David Loveday <davemech@yahoo.com>
2019-02-20 21:57:57 -0800

    format documents

--- family_life_frontend/src/components/landing/homePageHeading.js
+++ family_life_frontend/src/components/landing/homePageHeading.js
@@ -20,40 +20,40 @@ import {
   Visibility
 } from "semantic-ui-react";
 
- const HomepageHeading = ({ mobile }) => (
-    <Container text>
-      <Header
-        as="h1"
-        content="FamilyLife"
-        inverted
-        style={{
-          fontSize: mobile ? "2em" : "6em",
-          fontWeight: "bold",
-          marginBottom: -19,
-          marginTop: mobile ? "1.5em" : "2em"
-        }}
-      />
-      <Header
-        as="h2"
-        content="The best way to keep your family organized and unified!"
-        inverted
-        style={{
-          fontSize: mobile ? "1.5em" : "1.7em",
-          fontWeight: "normal",
-          marginTop: mobile ? "0.5em" : "1.5em"
-        }}
-      />
-      <NavLink to="login">
-        <Button primary size="huge">
-          Get Organized!
-          <Icon name="right arrow" />
-        </Button>{" "}
-      </NavLink>
-    </Container>
-  );
-  
-  HomepageHeading.propTypes = {
-    mobile: PropTypes.bool
-  };
+const HomepageHeading = ({ mobile }) => (
+  <Container text>
+    <Header
+      as="h1"
+      content="FamilyLife"
+      inverted
+      style={{
+        fontSize: mobile ? "2em" : "6em",
+        fontWeight: "bold",
+        marginBottom: -19,
+        marginTop: mobile ? "1.5em" : "2em"
+      }}
+    />
+    <Header
+      as="h2"
+      content="The best way to keep your family organized and unified!"
+      inverted
+      style={{
+        fontSize: mobile ? "1.5em" : "1.7em",
+        fontWeight: "normal",
+        marginTop: mobile ? "0.5em" : "1.5em"
+      }}
+    />
+    <NavLink to="login">
+      <Button primary size="huge">
+        Get Organized!
+        <Icon name="right arrow" />
+      </Button>{" "}
+    </NavLink>
+  </Container>
+);
 
-  export default HomepageHeading;
+HomepageHeading.propTypes = {
+  mobile: PropTypes.bool
+};
+
+export default HomepageHeading;
--- family_life_frontend/src/components/landing/mobileContainer.js
+++ family_life_frontend/src/components/landing/mobileContainer.js
@@ -23,78 +23,78 @@ import {
 } from "semantic-ui-react";
 
 class MobileContainer extends Component {
-    state = {};
-  
-    handleSidebarHide = () => this.setState({ sidebarOpened: false });
-  
-    handleToggle = () => this.setState({ sidebarOpened: true });
-  
-    render() {
-      const { children } = this.props;
-      const { sidebarOpened } = this.state;
-  
-      return (
-        <Responsive
-          as={Sidebar.Pushable}
-          maxWidth={Responsive.onlyMobile.maxWidth}
-        >
-          <Sidebar
-            as={Menu}
-            animation="push"
-            inverted
-            onHide={this.handleSidebarHide}
-            vertical
-            visible={sidebarOpened}
-          >
-            <Menu.Item as="a" active>
-              Home
-            </Menu.Item>
-            <NavLink to="/login">
-              <Menu.Item as="a">Log in</Menu.Item>
-            </NavLink>
-            <NavLink to="/register">
-              <Menu.Item as="a">Sign Up</Menu.Item>
-            </NavLink>
-          </Sidebar>
-          <Sidebar.Pusher dimmed={sidebarOpened}>
-            <Segment
-              inverted
-              textAlign="center"
-              style={{ minHeight: 350, padding: "1em 0em" }}
-              vertical
-            >
-              <Container>
-                <Menu inverted pointing secondary size="large">
-                  <Menu.Item onClick={this.handleToggle}>
-                    <Icon name="sidebar" />
-                  </Menu.Item>
-                  <Menu.Item position="right">
-                    <NavLink to="/login">
-                      <Button as="a" inverted>
-                        Log in
-                      </Button>
-                    </NavLink>
-                    <NavLink to="/register">
-                      {" "}
-                      <Button as="a" inverted style={{ marginLeft: "0.5em" }}>
-                        Sign Up
-                      </Button>
-                    </NavLink>
-                  </Menu.Item>
-                </Menu>
-              </Container>
-              <HomepageHeading mobile />
-            </Segment>
-  
-            {children}
-          </Sidebar.Pusher>
-        </Responsive>
-      );
-    }
-  }
-  
-  MobileContainer.propTypes = {
-    children: PropTypes.node
-  };
+  state = {};
 
-  export default MobileContainer;
+  handleSidebarHide = () => this.setState({ sidebarOpened: false });
+
+  handleToggle = () => this.setState({ sidebarOpened: true });
+
+  render() {
+    const { children } = this.props;
+    const { sidebarOpened } = this.state;
+
+    return (
+      <Responsive
+        as={Sidebar.Pushable}
+        maxWidth={Responsive.onlyMobile.maxWidth}
+      >
+        <Sidebar
+          as={Menu}
+          animation="push"
+          inverted
+          onHide={this.handleSidebarHide}
+          vertical
+          visible={sidebarOpened}
+        >
+          <Menu.Item as="a" active>
+            Home
+          </Menu.Item>
+          <NavLink to="/login">
+            <Menu.Item as="a">Log in</Menu.Item>
+          </NavLink>
+          <NavLink to="/register">
+            <Menu.Item as="a">Sign Up</Menu.Item>
+          </NavLink>
+        </Sidebar>
+        <Sidebar.Pusher dimmed={sidebarOpened}>
+          <Segment
+            inverted
+            textAlign="center"
+            style={{ minHeight: 350, padding: "1em 0em" }}
+            vertical
+          >
+            <Container>
+              <Menu inverted pointing secondary size="large">
+                <Menu.Item onClick={this.handleToggle}>
+                  <Icon name="sidebar" />
+                </Menu.Item>
+                <Menu.Item position="right">
+                  <NavLink to="/login">
+                    <Button as="a" inverted>
+                      Log in
+                    </Button>
+                  </NavLink>
+                  <NavLink to="/register">
+                    {" "}
+                    <Button as="a" inverted style={{ marginLeft: "0.5em" }}>
+                      Sign Up
+                    </Button>
+                  </NavLink>
+                </Menu.Item>
+              </Menu>
+            </Container>
+            <HomepageHeading mobile />
+          </Segment>
+
+          {children}
+        </Sidebar.Pusher>
+      </Responsive>
+    );
+  }
+}
+
+MobileContainer.propTypes = {
+  children: PropTypes.node
+};
+
+export default MobileContainer;
