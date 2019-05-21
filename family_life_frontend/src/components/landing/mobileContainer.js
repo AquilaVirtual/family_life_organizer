c5cb7077d235fea02ba0eabfc2e7ac8e17688f51
David Loveday <davemech@yahoo.com>
2019-05-21 13:30:01 -0700

    add media break point

--- family_life_frontend/src/components/landing/mobileContainer.js
+++ family_life_frontend/src/components/landing/mobileContainer.js
@@ -1,25 +1,38 @@
 import PropTypes from "prop-types";
 import React, { Component } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, withRouter } from "react-router-dom";
 
 import HomepageHeading from "./homePageHeading";
 import "../css/LandingPage.css";
 
 import {
+  Button,
   Container,
   Icon,
   Menu,
   Responsive,
   Segment,
-  Sidebar
+  Sidebar,
 } from "semantic-ui-react";
 
 class MobileContainer extends Component {
-  state = {};
+  state = {
+    sidebarOpened: false
+  };
 
   handleSidebarHide = () => this.setState({ sidebarOpened: false });
 
-  handleToggle = () => this.setState({ sidebarOpened: true });
+  handleToggle = () => this.setState({ sidebarOpened: !this.sidebarOpened });
+
+  logOut = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("name");
+    localStorage.removeItem("userId");
+    localStorage.removeItem("accountType");
+    localStorage.removeItem("username");
+    this.handleSidebarHide()
+    this.props.history.push("/");
+  };
 
   render() {
     const { children } = this.props;
@@ -41,12 +54,14 @@ class MobileContainer extends Component {
           <Menu.Item as="a" active>
             Home
           </Menu.Item>
-          <NavLink to="/login">
-            <Menu.Item as="a">Log in</Menu.Item>
+          <NavLink to="/users">
+            <Menu.Item as="a">Users</Menu.Item>
           </NavLink>
-          <NavLink to="/register">
-            <Menu.Item as="a">Sign Up</Menu.Item>
-          </NavLink>
+          <NavLink to="/settings">
+            <Menu.Item as="a">Settings</Menu.Item>
+          </NavLink>        
+            <Menu.Item as="a" onClick={this.logOut}>Logout</Menu.Item>
+         
         </Sidebar>
         <Sidebar.Pusher dimmed={sidebarOpened}>
           <Segment
@@ -57,9 +72,23 @@ class MobileContainer extends Component {
           >
             <Container>
               <Menu inverted pointing secondary size="large">
-                <Menu.Item onClick={this.handleToggle}>
+               {localStorage.getItem("token") ? (<Menu.Item onClick={this.handleToggle}>
                   <Icon name="sidebar" />
-                </Menu.Item>
+                </Menu.Item> ):(null )}
+
+               { localStorage.getItem("token") ? (null ):(<Menu.Item position="right">
+                  <NavLink to="/login">
+                    <Button as="a" inverted>
+                      Log in
+                    </Button>
+                  </NavLink>
+                  <NavLink to="/register">
+                    {" "}
+                    <Button as="a" inverted style={{ marginLeft: "0.5em" }}>
+                      Sign Up
+                    </Button>
+                  </NavLink>
+                </Menu.Item>)}
               </Menu>
             </Container>
             <HomepageHeading mobile />
@@ -76,4 +105,4 @@ MobileContainer.propTypes = {
   children: PropTypes.node
 };
 
-export default MobileContainer;
+export default withRouter(MobileContainer);
