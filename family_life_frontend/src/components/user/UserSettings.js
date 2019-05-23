300d7224160473af5045f283febba128f310a369
David Loveday <davemech@yahoo.com>
2019-05-22 23:29:30 -0700

    user settings component update

--- family_life_frontend/src/components/user/UserSettings.js
+++ family_life_frontend/src/components/user/UserSettings.js
@@ -1,4 +1,14 @@
 import React, { Component } from "react";
+
+import {
+    Segment,
+    Image,
+    Header,
+    Icon,
+    Confirm,
+    Button,
+  } from "semantic-ui-react";
+  
 import SiteHeader from "../header/SiteHeader";
 import Navbar from "../navbar/Navbar";
 
@@ -19,32 +29,101 @@ class UserSettings extends Component {
   }
 
   render() {
+    const { user, deleteUser, handleModalToggle } = this.props;
+    const { confirmDelete } = this.state;
+
     return (
-        
-      <div className="container">
-      <Navbar />
-        <SiteHeader name="settings" />
-        <div className="settings">
-          <div className="info-label">Username:</div>
-          <div className="info-data">{this.state.username}</div>
-          <button className="username-button" onClick={this.changeUsername}>
-            Change{" "}
-          </button>
+          <div className="container">
+       <Navbar />
+         <SiteHeader name="settings" />
+         <Segment
+        style={{
+          width: "18rem",
+          margin: "1rem auto"
+        }}
+        textAlign="center"
+      >
+        <Image
+          centered
+          src={
+            user && user.userImage
+              ? user.userImage
+              : "https://png.icons8.com/ios/100/000000/gender-neutral-user.png"
+          }
+          size="small"
+          circular
+        />
+        <Button
+          onClick={this.openImageUploader}
+          content="Change Image"
+          style={{ fontSize: ".7rem" }}
+        />        
+        <div id="image-uploader">
+          <input
+            type="file"
+            name="image"
+            value={this.state.image}
+            onChange={this.handleInputChange}
+          />
+          <button onClick={this.handleImageUpload}>Upload Image</button>
         </div>
-        <div className="settings">
-          <div className="info-label">Email:</div>
-          <div className="info-data_email"> {this.state.email}</div>
-          <button className="email-button" onClick={this.changeEmail}>
-            Change
-          </button>{" "}
-        </div>
-        <div className="settings">
-          Password: ******************{" "}
-          <button className="password-button" onClick={this.changePassword}>
-            Change
-          </button>{" "}
-        </div>
+        <Header as="h1">{user}</Header>
+        <p>{user}</p>
+        {/* Show the edit and delete icons if the logged in user's account type is primary.
+            Only a primary account holder can delete and/or add family members. 
+        */}
+        {localStorage.getItem("accountType") === "Primary" ||
+        localStorage.getItem("accountType") === "Spouse" ? (
+          <div>
+            <Icon
+              style={{ cursor: "pointer", fontSize: "1.4rem" }}
+              className="edit outline green"
+              onClick={this.toggleEdit}
+            />
+            <Icon
+              style={{ cursor: "pointer", fontSize: "1.4rem" }}
+              className="trash alternate outline red"
+              onClick={() => this.setState({ confirmDelete: true })}
+            />{" "}
+          </div>
+        ) : null}
+        <Confirm
+          open={confirmDelete}
+          size="mini"
+          content={`Are you sure you want to remove ${user} ?`}
+          onCancel={() => this.setState({ confirmDelete: false })}
+          onConfirm={() => {
+            deleteUser();
+            this.setState({ confirmDelete: false });
+          }}
+        />          
+      </Segment>
       </div>
+        
+    //   {/* <div className="container">
+    //   <Navbar />
+    //     <SiteHeader name="settings" />
+    //     <div className="settings">
+    //       <div className="info-label">Username:</div>
+    //       <div className="info-data">{this.state.username}</div>
+    //       <button className="username-button" onClick={this.changeUsername}>
+    //         Change{" "}
+    //       </button>
+    //     </div>
+    //     <div className="settings">
+    //       <div className="info-label">Email:</div>
+    //       <div className="info-data_email"> {this.state.email}</div>
+    //       <button className="email-button" onClick={this.changeEmail}>
+    //         Change
+    //       </button>{" "}
+    //     </div>
+    //     <div className="settings">
+    //       Password: ******************{" "}
+    //       <button className="password-button" onClick={this.changePassword}>
+    //         Change
+    //       </button>{" "}
+    //     </div>
+    //   </div> */}
     );
   }
 }
