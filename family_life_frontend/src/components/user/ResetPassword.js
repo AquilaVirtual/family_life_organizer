7012db1a21d1bdd6ccbed63e4e451a57e17f52e8
David Loveday <davemech@yahoo.com>
2019-06-14 20:48:30 -0700

    add Component for resetting password

---
+++ family_life_frontend/src/components/user/ResetPassword.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+
+class ResetPassword extends Component {
+    render() {
+        return (
+            <div>Reset Password</div>
+        )
+    }
+}
+
+export default ResetPassword;
