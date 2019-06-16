adacb9c5bf639e9887f3c37088a053b0eb4d722b
David Loveday <davemech@yahoo.com>
2019-06-15 17:24:15 -0700

    add state to reset password component

--- family_life_frontend/src/components/user/ResetPassword.js
+++ family_life_frontend/src/components/user/ResetPassword.js
@@ -3,6 +3,18 @@ import React, { Component } from "react";
 import "../css/ResetPassword.css";
 
 class ResetPassword extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      error: false,
+      errorMessage: ""
+    };
+  }
+  handleInputChange = event => {
+    event.preventDefault();
+    this.setState({ [event.target.name]: event.target.value });
+  };
   render() {
     return (
       <div className="reset-container">
@@ -10,17 +22,21 @@ class ResetPassword extends Component {
           <h1>Forgot your Password?</h1>
         </div>
         <div>
-          Please enter your email address below, and we'll send you and an email to reset your password.
+          Please enter your email address below, and we'll send you and an email
+          to reset your password.
         </div>
-        <div className="form-wrapper"> 
-          <label forHtml="">
-            Email address
-          </label>
-          <input type="text"  className="reset-input--control"/>
-          <input type="submit" value="Continue" className="btn-block"/>
-          <span className="danger">
-            You must enter a valid email address.
-          </span>
+        <div className="form-wrapper">
+          <label forHtml="">Email address</label>
+          <input
+            type="text"
+            placeholder="Email"
+            className="reset-input--control"
+            name="email"
+            value={this.state.email}
+            onChange={this.handleInputChange}
+          />
+          <input type="submit" value="Continue" className="btn-block" />
+          <span className="danger">You must enter a valid email address.</span>
         </div>
       </div>
     );
