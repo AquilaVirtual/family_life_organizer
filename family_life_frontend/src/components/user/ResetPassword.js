9396b6484468f0c1799db7ec9dd5ce90851da671
David Loveday <davemech@yahoo.com>
2019-06-19 11:26:16 -0700

    implement a method for email validation

--- family_life_frontend/src/components/user/ResetPassword.js
+++ family_life_frontend/src/components/user/ResetPassword.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { withRouter } from "react-router-dom"
+import { withRouter } from "react-router-dom";
 import axios from "axios";
 
 import "../css/ResetPassword.css";
@@ -31,36 +31,40 @@ class ResetPassword extends Component {
       current: "passwordsent"
     });
   };
-  resetPassword = e => {
-      if(this.state.email !=="") {
 
-    
-    e.preventDefault();
-    let email = {
-      email: this.state.email
-    };
-    console.log("Info", email);
-    axios
-      .post(`${backend}/api/user/forgotpassword`, email)
-      .then(response => {
-        console.log("Getting something", response);
-        this.setState({
-          email: ""
-        });
-        this.changeState();
-      })
-      .catch(err => {
-        // console.log(err)
-        this.setState({
-          error: true,
-          errorMessage: err.response.data.errorMessage
-        });
-      });
+  validateEmail = mail => {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+      return true;
     }
-    else {
-        this.setState({
-            empty: true
+    return false;
+  };
+  resetPassword = e => {
+    if (this.state.email !== "" && this.validateEmail(this.state.email)) {
+      e.preventDefault();
+      let email = {
+        email: this.state.email
+      };
+      console.log("Info", email);
+      axios
+        .post(`${backend}/api/user/forgotpassword`, email)
+        .then(response => {
+          console.log("Getting something", response);
+          this.setState({
+            email: ""
+          });
+          this.changeState();
         })
+        .catch(err => {
+          // console.log(err)
+          this.setState({
+            error: true,
+            errorMessage: err.response.data.errorMessage
+          });
+        });
+    } else {
+      this.setState({
+        empty: true
+      });
     }
   };
 
@@ -68,21 +72,28 @@ class ResetPassword extends Component {
     switch (this.state.current) {
       case "passwordsent":
         return (
-          <div className="instruction-container"> 
-            <header  className="instruction-header">
-              <h1 >Password request sent</h1>
+          <div className="instruction-container">
+            <header className="instruction-header">
+              <h1>Password request sent</h1>
             </header>
             <div className="content-wrapper">
-            <i className="fas fa-envelope-open-text"></i>
-            <div className="instruction">
-              <p>
-                We've emailed you instructions on how to reset your password. If
-                you don't see it, don't forget to check your spam folder.
-              </p>
-            </div>
+              <i className="fas fa-envelope-open-text" />
+              <div className="instruction">
+                <p>
+                  We've emailed you instructions on how to reset your password.
+                  If you don't see it, don't forget to check your spam folder.
+                </p>
+              </div>
             </div>
             <footer className="formFooter">
-              <button className="loginhelp-btn" onClick={()=>{this.props.history.push("/login")}}>Go to Log In</button>
+              <button
+                className="loginhelp-btn"
+                onClick={() => {
+                  this.props.history.push("/login");
+                }}
+              >
+                Go to Log In
+              </button>
             </footer>
           </div>
         );
@@ -90,36 +101,49 @@ class ResetPassword extends Component {
       default:
         return (
           <div className="instruction-container">
-          <header className="instruction-header">
+            <header className="instruction-header">
               <h1>Forgot your Password?</h1>
             </header>
             <div className="content-wrapper">
-            <i className="fas fa-envelope-open-text"></i>
-            <div className="instruction">
-              Please enter your email address below, and we'll send you an
-              email to reset your password.
-            </div>
-            </div>
-            <div className="form-wrapper">   
-                <div className="reset-input-wrapper">  
-              <input
-              required
-                type="text"
-                placeholder="Email"
-                className="reset-input--control"
-                name="email"
-                value={this.state.email}
-                onChange={this.handleInputChange}
-              />
+              <i className="fas fa-envelope-open-text" />
+              <div className="instruction">
+                Please enter your email address below, and we'll send you an
+                email to reset your password.
               </div>
-              { this.state.empty ? ( <span className="danger">
-                You must enter a valid email address.
-              </span>):(null)          
-              }
+            </div>
+            <div className="form-wrapper">
+              <div className="reset-input-wrapper">
+                <input
+                  required
+                  type="text"
+                  placeholder="Email"
+                  className="reset-input--control"
+                  name="email"
+                  value={this.state.email}
+                  onChange={this.handleInputChange}
+                />
+              </div>
+              {this.state.empty ? (
+                <span className="danger">
+                  You must enter a valid email address.
+                </span>
+              ) : null}
               <footer className="formFooter">
-              <button onClick={()=>{this.props.history.push("/login")}} className="loginhelp-btn left">Back</button>           
-              <button onClick={this.resetPassword} className="loginhelp-btn right" >Submit</button>   
-              </footer>          
+                <button
+                  onClick={() => {
+                    this.props.history.push("/login");
+                  }}
+                  className="loginhelp-btn left"
+                >
+                  Back
+                </button>
+                <button
+                  onClick={this.resetPassword}
+                  className="loginhelp-btn right"
+                >
+                  Submit
+                </button>
+              </footer>
             </div>
           </div>
         );
